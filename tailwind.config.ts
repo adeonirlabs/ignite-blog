8e9133553623e6c03ef3c74c6b0505ec9abde971
Adeonir Kohl <adeonir@gmail.com>
2023-10-11 11:08:33 -0300

    update gray colors based on design

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,6 +7,21 @@ const config: Config = {
       backgroundImage: {
         header: 'url("/assets/header.jpg")',
       },
+      colors: {
+        gray: {
+          50: '#f9fafb',
+          100: '#e7edf4',
+          200: '#c4d4e3',
+          300: '#afc2d4',
+          400: '#7b96b2',
+          500: '#3a536b',
+          600: '#1c2f41',
+          700: '#112131',
+          800: '#0b1b2b',
+          900: '#071422',
+          950: '#040f1a',
+        },
+      },
     },
   },
   plugins: [],
