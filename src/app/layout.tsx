825035684a826d39c18b71b3e26415c738f5b0f5
Adeonir Kohl <adeonir@gmail.com>
2023-10-12 07:12:22 -0300

    add header to root layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <body className={nunito.className}>
-        <header className="bg-header flex items-center justify-center bg-red-500 bg-cover bg-center bg-no-repeat pb-36 pt-16">
+        <header className="flex items-center justify-center bg-red-500 bg-header bg-cover bg-center bg-no-repeat pb-36 pt-16">
           <Image src="/assets/logo.svg" alt="GitHub Blog" width={150} height={100} priority />
         </header>
         {children}
