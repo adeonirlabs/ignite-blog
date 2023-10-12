6eda4305ce14a0557e93e84ff2f745da972b7624
Adeonir Kohl <adeonir@gmail.com>
2023-10-12 11:57:44 -0300

    remove header from root layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import './globals.css'
 
 import type { Metadata } from 'next'
 import { Nunito } from 'next/font/google'
-import Image from 'next/image'
+import type { ReactNode } from 'react'
 
 const nunito = Nunito({ subsets: ['latin'] })
 
@@ -11,15 +11,10 @@ export const metadata: Metadata = {
   description: 'Um blog criado por Adeonir Kohl como desafio de react, usando issues do GitHub para as postagens',
 }
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
-      <body className={nunito.className}>
-        <header className="flex items-center justify-center bg-red-500 bg-header bg-cover bg-center bg-no-repeat pb-36 pt-16">
-          <Image src="/assets/logo.svg" alt="GitHub Blog" width={150} height={100} priority />
-        </header>
-        {children}
-      </body>
+      <body className={nunito.className}>{children}</body>
     </html>
   )
 }
